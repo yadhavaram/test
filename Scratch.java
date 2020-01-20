163196f8a71de3a5464577d0c1dac8ca739163c0
YadhavaRam Elango <eyadhavaram@athenahealth.com>
2020-01-20 12:31:27 +0530

    first version of java code

---
+++ Scratch.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+class Scratch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for a");
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
